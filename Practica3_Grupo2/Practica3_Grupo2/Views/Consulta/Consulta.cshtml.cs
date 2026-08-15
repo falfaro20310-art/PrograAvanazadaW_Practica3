c724961dc01fe58ec80064271c1b0c5c721eb78d
María José <marjose908@gmail.com>
2026-08-14 20:57:38 -0600

    Views Registro y Consulta

---
+++ Practica3_Grupo2/Practica3_Grupo2/Views/Consulta/Consulta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica3_Grupo2.Views.Consulta
+{
+    public class ConsultaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Practica3_Grupo2/Practica3_Grupo2/Views/Registro/Registro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica3_Grupo2.Views.Registro
+{
+    public class RegistroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
