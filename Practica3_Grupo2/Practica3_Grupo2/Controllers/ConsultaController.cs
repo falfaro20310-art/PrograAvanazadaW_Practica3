843e774e714c23d9a30162db5a63b70811cedfc5
María José <marjose908@gmail.com>
2026-08-14 22:02:43 -0600

    SP

---
+++ Practica3_Grupo2/Practica3_Grupo2/Controllers/ConsultaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica3_Grupo2.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica3_Grupo2/Practica3_Grupo2/Controllers/RegistroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica3_Grupo2.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
