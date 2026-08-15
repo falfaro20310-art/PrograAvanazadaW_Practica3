4c3297ad6781101283f12e855aead2ca3095bba4
María José <marjose908@gmail.com>
2026-08-14 19:36:34 -0600

    Proyecto WEB y API Creados

---
+++ Practica3_Grupo2/Practica3_Grupo2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica3_Grupo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
